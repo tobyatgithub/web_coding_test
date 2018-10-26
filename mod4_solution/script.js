a452853d4a952223b21ac1882e277a8d8bb50011
tobyatgithub <toby.fangyuan@gmail.com>
2018-10-25 23:41:19 -0400

    add window

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -33,8 +33,9 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function (names) {
+	
 
 	// STEP 10:
 	// Loop over the names array and say either 'Hello' or "Good Bye"
@@ -64,7 +65,5 @@ WARNING!!! WARNING!!!
 	    helloSpeaker.speak(names[i])
 	  }
 	}
-
-
-})();
+})(names);
 
